2f602595de472faa4a32b4d9aefad5822dc23969
daniela_bravo <danielabravorueda@gmail.com>
2025-11-24 13:44:30 -0400

    modificando el archivo .env

--- conftest.py
+++ conftest.py
@@ -16,5 +16,4 @@ def get_token():
 
 
 
-
     
--- utils/config.py
+++ utils/config.py
@@ -1,14 +1,14 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 BASE_URI = os.getenv("BASE_URI")
 USER_EMAIL = os.getenv("USER_EMAIL")
 USER_PASSWORD = os.getenv("USER_PASSWORD")
 
 
-#Borrar luego
-print("BASE_URI:", BASE_URI)
-print("USER_EMAIL:", USER_EMAIL)
-print("USER_PASSWORD:", USER_PASSWORD)
+if not USER_EMAIL or not USER_PASSWORD:
+    raise RuntimeError(" Error: No se cargaron las variables desde .env")
